705e4499b449bbbbd9a01ffdd3151bad713c1201
Chris Carrier <ctcarrier@gmail.com>
2011-12-05 23:06:46 -0800

    Adding LiftJsonSupport

---
+++ spray-base/src/test/scala/cc/spray/typeconversion/LiftJsonSupportSpec.scala
@@ -0,0 +1,43 @@
+package cc.spray.typeconversion
+
+import org.specs2.Specification
+import cc.spray.http._
+import MediaTypes._
+import net.liftweb.json.DefaultFormats
+
+/**
+ * @author chris_carrier
+ * @version 9/26/11
+ */
+
+class LiftJsonSupportSpec extends Specification with LiftJsonSupport {
+
+  implicit val formats = DefaultFormats
+
+  case class Employee(fname: String, name: String, age: Int, id: Long, boardMember: Boolean)
+
+  val employeeA = Employee("Frank", "Smith", 42, 12345, false)
+  val employeeAJson = """{
+    "fname":"Frank",
+    "name":"Smith",
+    "age":42,
+    "id":12345,
+    "boardMember": false
+  }"""
+
+  def is = args() ^
+    "The LiftJsonSupport should" ^
+    "provide unmarshalling support" ! unmarshallTest() ^
+    "and return 404 for a non-existent resource" ! marshallTest() ^
+    end
+
+    def unmarshallTest() = {
+      (HttpContent(`application/json`, employeeAJson).as[Employee] must be equalTo
+              Right(Employee("Frank", "Smith", 42, 12345, false)))
+    }
+
+  def marshallTest() = {
+      employeeA.toHttpContent mustEqual HttpContent(ContentType(`application/json`), employeeAJson)
+    }
+
+}
